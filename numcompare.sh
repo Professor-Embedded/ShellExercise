f223bb126d27039e321394de921aecb33a5ae684
Professor-Embedded <61ashishparmar@gmail.com>
2025-02-03 13:43:46 +0530

    Modified the code for handling floating point numbers

--- numcompare.sh
+++ numcompare.sh
@@ -9,19 +9,19 @@ read -p "Enter value of y: " y
 #check for the valid input type (num)
 if [[ (($x =~ ^[+-]?[0-9]+([.][0-9]+)?$)) && (($y =~ ^[+-]?[0-9]+([.][0-9]+)?$)) ]]
 	then
-	if [[ $x -lt $y ]]
+	if [[ "$(echo "$x < $y" | bc -l )" -eq 1  ]]
 		then
 		echo "$x is less than $y"
 		
-	elif [[ $x -gt $y ]]
+	elif [[ "$(echo "$x > $y" | bc -l)" -eq 1 ]]
 		then
 		echo "$x is greater than $y"
 		
 	else
 		echo "$x is equal to $y"
 		fi
-	fi
 
+else
 echo "Not a valid input type (Both the values should be numbers only!!)"
 exit 1
-
+fi
