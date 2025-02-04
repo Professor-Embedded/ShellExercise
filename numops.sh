0b6844f2172ffbc42d02062d20f8051e4b1c4cf7
Professor-Embedded <61ashishparmar@gmail.com>
2025-02-04 15:07:44 +0530

    Modification in code: Functions added instead of direct calculation

--- numops.sh
+++ numops.sh
@@ -2,20 +2,64 @@
 
 #Goal: take two numbers as input and print sum, difference, muliplication, division
 
+
+validity(){
+	num1=$1
+	num2=$2
+
+	if [[ (($num1 =~ ^[+-]?[0-9]+([.][0-9]+)?$)) && (($num2 =~ ^[+-]?[0-9]+([.][0-9]+)?$)) ]] 
+		then
+		return 1
+	else
+		return 0
+	fi
+}
+
+sum(){
+	num1=$1
+	num2=$2
+	echo "$num1 + $num2" | bc -l
+}
+
+difference(){
+	num1=$1
+	num2=$2
+	echo "$num1 - $num2" | bc -l
+}
+
+multiplication(){
+	num1=$1
+	num2=$2
+	echo "$num1 * $num2" | bc -l
+}
+
+division(){
+	num1=$1
+	num2=$2
+	echo "$num1 / $num2" | bc -l
+}
+
+
 #taking 2 number as input from user
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
 
 #checking for numbers
-if [[ (($num1 =~ ^[+-]?[0-9]+([.][0-9]+)?$)) && (($num2 =~ ^[+-]?[0-9]+([.][0-9]+)?$)) ]]
+validity $num1 $num2
+
+if [[ $? -eq 1 ]]
 	then
-	echo "Sum: $(echo "$num1 + $num2" | bc -l)"
-	echo "Difference: $(echo "$num1 - $num2" | bc -l)"
-	echo "Multiplication: $(echo "$num1 * $num2" | bc -l)"
-	echo "Division: $(echo "$num1 / $num2" | bc -l)"
-	exit 0
-	fi
-echo "Not a valid input (Both the entered values should be numbers only!!"
-exit 1
+	sum=$(sum $num1 $num2)
+	echo "Sum: $sum"
+	diff=$(difference $num1 $num2)
+	echo "Difference: $diff"
+	multi=$(multiplication $num1 $num2)
+	echo "Multiplication: $multi"
+	div=$(division $num1 $num2)
+	echo "Division: $div"
+else
+	echo "Not a valid input (Both the entered values should be numbers only!!"
+	exit 1
+fi
 
 
