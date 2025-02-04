91d2826340951a027c7538a4ff2606addd943768
Professor-Embedded <61ashishparmar@gmail.com>
2025-02-04 15:23:01 +0530

    Modification: implemented using for loop

--- fileRead.sh
+++ fileRead.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+#Goal: reading only third character of each line
+
 read -p "File path:" file
-file=$(cat $file)
+
+<<oldcode
+#Used < instead of cat 
+file=$(<"$file")
 for line in $file
 	do
 		echo $line | cut -c3
 	done
+oldcode
+
+
+#Using while loop
+
+while read -r line
+	do
+		echo $line | cut -c3
+	done < "$file"
+
+
