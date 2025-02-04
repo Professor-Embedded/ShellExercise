f72510a7d6a59eec239bfdda8ffa8020d0531bf6
Professor-Embedded <61ashishparmar@gmail.com>
2025-02-04 16:54:45 +0530

    Error solved of line 5

--- expressionEval.sh
+++ expressionEval.sh
@@ -2,11 +2,36 @@
 
 #Goal: Taking mathametical expression as input and evaluates it
 
-
+<<old
 read -p "Enter a valid mathematical expression: " exp
 
+if [[ $exp =~ ^[0-9\+\-\*\/\(\)\. ]+$ ]]
+	then 
 	ans=$(echo "$exp" | bc -l )
 	printf %.3f $ans
 	echo
+else
+	echo "Not a valid exp"
+fi
+old
 
+
+
+
+
+read -p "Enter a valid mathematical expression: " exp
+
+# Validate the expression using a properly formatted regex
+if [[ $exp =~ ^[0-9\+\-\*/\(\)\.[:space:]]+$ ]]; then
+    ans=$(echo "$exp" | bc -l)
+
+    # Check if bc produced a valid output
+    if [[ -z "$ans" || "$ans" == "syntax error" ]]; then
+        echo "Invalid mathematical expression."
+    else
+        printf "%.3f\n" "$ans"
+    fi
+else
+    echo "Not a valid expression."
+fi
 
--- oddnumbers.sh
+++ oddnumbers.sh
@@ -2,22 +2,20 @@
 
 # Goal: Printing odd numbers between 1 to 100
 
-<<oldversion
 
-num1=1
-num2=100
+#num1=1
+#num2=100
 
-while [[ $num1 -le $num2 ]]
-do
-	temp=$(($num1 % 2))
-	if [[ $temp -ne 0 ]]
-		then
-			echo $num1
-		fi
-	num1=$(($num1+1))
-done
+#while [[ $num1 -le $num2 ]]
+#do
+#	temp=$(($num1 % 2))
+#	if [[ $temp -ne 0 ]]
+#		then
+#			echo $num1
+#		fi
+#	num1=$(($num1+1))
+#done
 
-oldversion
 
 for (( i=1; i<=100; i++ ))
 	do
