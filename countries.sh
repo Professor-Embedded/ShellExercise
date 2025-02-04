fbc93df54598a86f9e8b9939a07621f38a021b09
Professor-Embedded <61ashishparmar@gmail.com>
2025-02-04 15:33:00 +0530

    Modification: Using loop

--- countries.sh
+++ countries.sh
@@ -2,16 +2,23 @@
 
 #Goal: prints countries from index 3 to 7 (inclusive) from a given list
 file=countries.txt
-i=3
-j=7
+LowerInd=3
+UpperInd=7
+
+<<oldcode
 #while[[ $i -le $j ]]
 #	do
 #	awk 'NR==${i}{ print}' $file
 #	done
 
-awk 'NR==3{ print; exit }' $file
-awk 'NR==4{ print; exit }' $file
-awk 'NR==5{ print; exit }' $file
-awk 'NR==6{ print; exit }' $file
-awk 'NR==7{ print; exit }' $file
+#awk 'NR==3{ print; exit }' $file
+#awk 'NR==4{ print; exit }' $file
+#awk 'NR==5{ print; exit }' $file
+#awk 'NR==6{ print; exit }' $file
+#awk 'NR==7{ print; exit }' $file
+oldcode
 
+for ((i=$LowerInd; i <=$UpperInd; i++))
+	do
+		awk -v line=$i 'NR==line { print; exit }' $file
+	done
